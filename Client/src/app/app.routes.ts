871cada069e685565fb4ef2bc9f784566a2cefeb
Priyanjith N <jithpriyan2006@gmail.com>
2024-10-31 18:05:01 +0530

    detail page for blog is complete

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { HomePageComponent } from './features/home/home-page/home-page.component
 import { AddEditBolgFormComponent } from './shared/components/add-edit-bolg-form/add-edit-bolg-form.component';
 import { LandingComponent } from './shared/components/landing/landing.component';
 import { MyBlogsComponent } from './shared/components/my-blogs/my-blogs.component';
+import { BlogDetailComponent } from './shared/components/blog-detail/blog-detail.component';
 
 export const routes: Routes = [
     {
@@ -48,6 +49,10 @@ export const routes: Routes = [
             {
                 path: "editblog/:blogId",
                 component: AddEditBolgFormComponent
+            },
+            {
+                path: "blog/:blogId",
+                component: BlogDetailComponent
             }
         ]
     }
